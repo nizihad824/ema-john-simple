cd09c8606795766c5ac40d2863d2c6cccb12f81d
nizihad824 <mdnurulislam97@gmail.com>
2022-03-29 18:27:26 +0200

    add summary in cart done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = (props) => {
-    const {cart}=props
+    const {cart}=props;
+    console.log(cart);
+    let total=0;
+    let shipping=0;
+
+    for(const product of cart ){
+        total =total+product.price;
+        shipping=shipping+product.shipping
+
+    }
+    const tax =parseFloat((total * 0.1).toFixed(2))
+    const grandTotal=tax+total+shipping
+
+
     return (
-        <div>
+        <div className='cart'>
             <h2>Order Summary</h2>
-              <p>Selected items:{cart.length}</p>
+            <p>Selected items:{cart.length}</p>
+            <p>Total Price:${total}</p>
+            <p>Total Shipping:${shipping}</p>
+            <p>Tax :${tax}</p>
+            <h5>Grand Total:${grandTotal}</h5>
             
         </div>
     );
