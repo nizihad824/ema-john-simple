8ea5e3705e69b47777bb16a33ef094fed4135fc0
nizihad824 <mdnurulislam97@gmail.com>
2022-04-11 13:39:49 +0200

    remove_Handler added

--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -1,7 +1,10 @@
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './ReviewItem.css'; 
 
 const ReviewItem = (props) => {
+    const {product, handleRemoveProduct} = props;
     const {name, img, price, shipping, quantity} = props.product;
     return (
         <div className='review-item'>
@@ -18,7 +21,7 @@ const ReviewItem = (props) => {
                 <p><small>Quantity: {quantity}</small></p>
             </div>
             <div className="delete-container">
-               <button>Delete</button>
+               <button onClick={()=>handleRemoveProduct(product)} className='delete-button'><FontAwesomeIcon className='delete-icon' icon={faTrashAlt}></FontAwesomeIcon></button>
             </div>
         </div>
     </div>
