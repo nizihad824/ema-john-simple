2883f6ac9124b9877fba388bed33c6e7a57c6abd
nizihad824 <mdnurulislam97@gmail.com>
2022-04-22 14:55:33 +0200

    onClick handler for sign out

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Inventory from './components/Inventory/Inventory';
 import About from './components/About/About';
 import Login from './components/Login/Login';
 import SignUp from './components/SignUp/SignUp';
+import RequireAuth from './components/RequireAuth/RequireAuth';
 
 function App() {
   return (
@@ -18,7 +19,12 @@ function App() {
         <Route path='/' element={<Shop></Shop>}></Route>
         <Route path='/shop' element={<Shop></Shop>}></Route>
         <Route path='/orders' element={<Orders></Orders>}></Route>
-        <Route path ='/inventory' element={<Inventory></Inventory>}></Route>
+
+        <Route path ='/inventory' element={
+          <RequireAuth>
+            <Inventory></Inventory>
+          </RequireAuth>
+        }></Route>
         <Route path='/about' element={<About></About>}></Route>
         <Route path ='/login' element={<Login></Login>}></Route>
         <Route path ='/sign-up' element={<SignUp></SignUp>}></Route>
