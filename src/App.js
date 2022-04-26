ed9fbf0e2ce1babebbd1c956cd5ea33e37c7a712
nizihad824 <mdnurulislam97@gmail.com>
2022-04-26 02:15:28 +0200

    click on procced shipping navigate to shipment component

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import About from './components/About/About';
 import Login from './components/Login/Login';
 import SignUp from './components/SignUp/SignUp';
 import RequireAuth from './components/RequireAuth/RequireAuth';
+import Shipment from './components/Shipment/Shipment';
 
 function App() {
   return (
@@ -23,6 +24,11 @@ function App() {
         <Route path ='/inventory' element={
           <RequireAuth>
             <Inventory></Inventory>
+          </RequireAuth>
+        }></Route>
+         <Route path="/shipment" element={
+          <RequireAuth>
+            <Shipment></Shipment>
           </RequireAuth>
         }></Route>
         <Route path='/about' element={<About></About>}></Route>
